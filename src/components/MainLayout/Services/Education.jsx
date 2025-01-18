633f934dd02fd355e4b47285068e8abef0a374c5
Miheer Gautam <miheergautam04@gmail.com>
2025-01-18 11:46:42 +0530

    Update the education details

--- src/components/MainLayout/Services/Education.jsx
+++ src/components/MainLayout/Services/Education.jsx
@@ -5,18 +5,18 @@ export default function Education() {
       <div className="p-4">
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center pt-2 px-2 text-neutral-300 gap-2">
           <h1 className="text-lg md:text-2xl font-semibold text-white">
-            JK LAKSHMIPAT UNIVERSITY
+            INDIAN INSTITUTE OF TECHNOLOGY KANPUR
           </h1>
-          <h1 className="text-sm md:text-base text-right">Jaipur, India</h1>
+          <h1 className="text-sm md:text-base text-right">Kanpur, India</h1>
         </div>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center px-2 text-neutral-300 gap-2">
           <h1 className="italic">
-            Bachelor of Engineering - Major in{" "}
+            Exchange Student in{" "}
             <span className="text-cust-red font-semibold">
-              Computer Science
+              Computer Science and Engineering
             </span>
           </h1>
-          <h1 className="text-sm md:text-base">August 2022 - Present</h1>
+          <h1 className="text-sm md:text-base">January 2025 - Present</h1>
         </div>
       </div>
       
@@ -30,12 +30,31 @@ export default function Education() {
         </div>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center px-2 text-neutral-300 gap-2">
           <h1 className="italic">
-            Semester Exchange Program in{" "}
+            Visiting Student in{" "}
             <span className="text-cust-red font-semibold">
               Computer Science and Engineering
             </span>
           </h1>
           <h1 className="text-sm md:text-base">January 2024 - April 2024</h1>
+        </div>
+      </div>
+      
+      {/* First Education Block */}
+      <div className="p-4">
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center pt-2 px-2 text-neutral-300 gap-2">
+          <h1 className="text-lg md:text-2xl font-semibold text-white">
+            JK LAKSHMIPAT UNIVERSITY
+          </h1>
+          <h1 className="text-sm md:text-base text-right">Jaipur, India</h1>
+        </div>
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center px-2 text-neutral-300 gap-2">
+          <h1 className="italic">
+            Bachelor of Engineering - Major in{" "}
+            <span className="text-cust-red font-semibold">
+              Computer Science
+            </span>
+          </h1>
+          <h1 className="text-sm md:text-base">August 2022 - Present</h1>
         </div>
       </div>
     </div>
