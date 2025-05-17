8bbb967ac6f6052fdb93e2a0f0a51b02e492b321
Miheer Gautam <miheergautam04@gmail.com>
2025-05-17 19:47:07 +0530

    feat: UI and content update

--- src/components/MainLayout/WorkSection/WorkSection.jsx
+++ src/components/MainLayout/WorkSection/WorkSection.jsx
@@ -2,8 +2,8 @@ import WorkTopBar from "../WorkSection/WorkTopBar";
 
 export default function WorkSection() {
   return (
-    <div id="projects" className="flex flex-col min-h-screen items-center w-full h-full gap-10 px-4 md:px-0">
-      <div className="flex flex-col items-center justify-center text-cust-red w-full max-w-7xl font-bold text-xl mb-4">
+    <div id="projects" className="flex flex-col items-center gap-10 px-4 md:px-0">
+      <div className="flex flex-col items-center justify-center text-cust-red w-full max-w-7xl font-bold text-xl my-20">
         <h1 className="text-white text-5xl md:text-7xl font-semibold px-3 py-2 text-center">
           <span className="text-cust-red font-bold">What </span>I Do !
         </h1>
--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import HeroSection from "../components/MainLayout/HeroSection";
 import Services from "../components/MainLayout/Services/Services";
 import WorkSection from "../components/MainLayout/WorkSection/WorkSection";
@@ -9,11 +8,15 @@ import Footer from "../components/MainLayout/Footer/Footer";
 
 export default function MainLayout() {
   return (
-    <div className="flex flex-col font-clash-display">
-        <HeroSection />
-        <Services />
-        <WorkSection />
-        <Contact />
+    <div className="flex flex-col space-y-32 font-clash-display">
+      <HeroSection />
+      <hr className="border-t border-neutral-700 my-10 mx-auto w-11/12" />
+      <Services />
+      <hr className="border-t border-neutral-700 my-10 mx-auto w-11/12" />
+      <WorkSection />
+      <hr className="border-t border-neutral-700 my-10 mx-auto w-11/12" />
+      <Contact />
+
       <Footer />
     </div>
   );
